1d2dfeef9e9b7f0ec15bad69a8bc78497e9775ca
ruirui.ding <dingrui_5988@163.com>
2021-03-15 23:54:27 +0800

    create project init

---
+++ src/components/ColumnLIst.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul v-for="item of list" :key="item.id">
+    <img src="item.avatar" alt="item.title" />
+    <h5>{{ item.title }}</h5>
+    <p>{{ item.description }}</p>
+    <a href="#">进入专栏</a>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: "ColumnList",
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true,
+    },
+  },
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
