52b1add56e26215a482ae069f42d8da4e9eef67e
ruirui.ding <dingrui_5988@163.com>
2021-03-17 23:12:44 +0800

    update json

--- src/components/ColumnLIst.vue
+++ src/components/ColumnLIst.vue
@@ -1,10 +1,16 @@
 <template>
-  <ul v-for="item of list" :key="item.id">
-    <img :src="item.avatar" alt="item.title" />
-    <h5>{{ item.title }}</h5>
-    <p>{{ item.description }}</p>
-    <a href="#">进入专栏</a>
-  </ul>
+  <div class="row">
+    <div v-for="item of list" :key="item.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img class="rounded-circle border border-light w-25 my-3" :src="item.avatar" alt="item.title" />
+          <h5 class="card-title">{{ item.title }}</h5>
+          <p class="card-text text-left">{{ item.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
